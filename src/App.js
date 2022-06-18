160f3eb9e7e2af39c174ee34f529a60670d88803
sanjayprasad s <sanjayprasads@sanjayprasads-MacBook-Air.local>
2022-06-18 11:28:22 +0530

    first build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import Unity, { UnityContext } from "react-unity-webgl";
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+// const unityContext = new UnityContext({
+//   loaderUrl: "/build/build1.loader.js",
+//   dataUrl: "/build/build1.data.unityweb",
+//   frameworkUrl: "/build/build1.framework.js.unityweb",
+//   codeUrl: "/build/build1.wasm.unityweb",
+// });
 
 function App() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "/build/build2.loader.js",
+    dataUrl: "/build/build2.data",
+    frameworkUrl: "/build/build2.framework.js",
+    codeUrl: "/build/build2.wasm",
+  });
+
+  // unityContext.on("Senddata", function (eventdata, eventname) {
+  //   alert("this is true !");
+  //   //questcompletion();
+  //   console.log("eventdata : " + String(eventdata));
+  //   console.log(eventname);
+  // });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +40,15 @@ function App() {
         >
           Learn React
         </a>
+        {/* <Unity
+          unityContext={unityContext}
+          style={{ width: 800, height: 600 }}
+        /> */}
+        <Unity
+          className="unityy"
+          unityProvider={unityProvider}
+          // style={{ width: 800, height: 600 }}
+        />
       </header>
     </div>
   );
